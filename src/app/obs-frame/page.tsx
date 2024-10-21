48ead6d26b1a4ae35c877affc1b2ca50758b5df7
ch4dwick <omar@xld.finance>
2024-10-21 15:54:36 +0800

    State before FlightTelemetry Object introduction

--- src/app/obs-frame/page.tsx
+++ src/app/obs-frame/page.tsx
@@ -15,19 +15,21 @@ const rootUrl = 'https://6594-182-18-225-92.ngrok-free.app/api/v1/datarefs';
 const ws = new WebSocket('wss://6594-182-18-225-92.ngrok-free.app/api/v1');
 
 export default function Page() {
-    // const [flight] = useState<Flight>({
-    //     depICAO: "RPLL",
-    //     arrICAO: "RPVM",
-    //     maxIAS: 100,
-    //     crsAlt: 30000,
-    //     transAlt: 8000
-    // });
 
+    // sim/cockpit2/gauges/indicators/altitude_ft_pilot
     const [currAltId, setCurrAltId] = useState<number>(0);
+    // sim/cockpit2/gauges/indicators/airspeed_kts_pilot
     const [pilotAirspeedId, setPilotAirspeedId] = useState<number>(0);
+    // sim/cockpit2/gauges/indicators/true_airspeed_kts_pilot
     const [pilotTrueAirspeedId, setPilotTrueAirspeedId] = useState<number>(0);
+    // sim/cockpit2/gauges/indicators/heading_AHARS_deg_mag_pilot
     const [compassHeadingDegId, setCompassHeadingDegId] = useState<number>(0);
+    // sim/cockpit2/gauges/indicators/compass_heading_deg_mag
     const [compassMagneticHeadingId, setCompassMagneticHeadingId] = useState<number>(0);
+    // sim/cockpit2/radios/indicators/fms_distance_to_tod_pilot
+    const [distanceToTODId, setDistanceToTODId] = useState<number>(0);
+    // sim/cockpit2/radios/indicators/fms_tod_before_distance_pilot
+    const [distanceAfterTODId, setDistanceAfterTODId] = useState<number>(0);
 
     // TODO: Move this to an object.
     const [dataRefs, setDataRefs] = useState<DataRefsResponse>({ data: [] });
@@ -50,113 +52,125 @@ export default function Page() {
         let dataRef = [];
         if (dataRefs.data.length > 0) {
             dataRef = JSONPath({ path: `$.data[?(@. name == '${name}')]`, json: dataRefs });
-            console.log(`dataref ${name} ${JSON.stringify(dataRef[0].id)}`);
+            console.log(`${name} ${JSON.stringify(dataRef[0].id)}`);
         }
         return (dataRefs.data.length > 0) ? dataRef[0].id : 0;
     }, [dataRefs]);
 
-    const getUpdatedDataRefWs = useCallback((source: object, id: number): number => {
-        console.log(`getUpdatedDataRefWs(${id})`);
-        let dataRef = [0];
-        if (source !== undefined) {
-            dataRef = JSONPath({ path: `$.${id}`, json: source });
-            console.log(`dataref ${id} ${JSON.stringify(dataRef[0])}`);
-        }
-        return dataRef[0];
-    }, []);
+    const getUpdatedDataRefWs = (source: object, id: number, fallBackVal: number): number => {
+        console.log(`getUpdatedDataRefWs(): ${id}`);
+        const dataRef = JSONPath({ path: `$.${id}`, json: source });
+        console.log(`${id} ${fallBackVal} ${dataRef[0]}`);
+        return dataRef[0] ?? fallBackVal;
+    };
 
     const subscribeToDataRefs = useCallback(async () => {
-        ws.addEventListener('open', () => {
-            console.log('Connected to server');
+        // ws.addEventListener('open', () => {
+        //     console.log('Connected to server');
+        //     const payload: DataRefWsRequest = {
+        //         req_id: 1,
+        //         type: 'dataref_subscribe_values',
+        //         params: {
+        //             datarefs: [
+        //                 { id: currAltId },
+        //                 { id: pilotAirspeedId },
+        //                 // { id: pilotTrueAirspeedId },
+        //                 { id: compassHeadingDegId },
+        //                 { id: compassMagneticHeadingId }
+        //             ]
+        //         }
+        //     }
+        //     console.log(`Final WS Payload: ${JSON.stringify(payload)}`)
+        //     ws.send(JSON.stringify(payload));
+        // });
+        // ws.addEventListener("message", (event) => {
+        //     // console.log("Message from server ", JSON.parse(event.data));
+        //     const json = JSON.parse(event.data);
+        //     if ("result" === json.type) {
+        //         console.log(`Subscribed values ${json.success}!`);
+        //     }
+        //     if ("dataref_update_values" === json.type) {
+        //         setCurrAltFt(getUpdatedDataRefWs(json.data, currAltId));
+        //         setCurrSpd(getUpdatedDataRefWs(json.data, pilotAirspeedId));
+        //         setCurrHdg(getUpdatedDataRefWs(json.data, compassHeadingDegId));
+        //         setCurrMagHdg(getUpdatedDataRefWs(json.data, compassMagneticHeadingId));
+        //     }
+        // });
+    }, [compassHeadingDegId, compassMagneticHeadingId, currAltId, getUpdatedDataRefWs, pilotAirspeedId])
+
+
+    const connectWs = () => {
+        console.log('connectWs()');
+        if (ws.readyState !== ws.OPEN) {
+            ws.addEventListener('open', () => {
+                console.log('Connected to server');
+            });
+        }
+        if (ws.readyState === ws.OPEN) {
             const payload: DataRefWsRequest = {
                 req_id: 1,
                 type: 'dataref_subscribe_values',
                 params: {
                     datarefs: [
-                        { id: 1608290516016 },
-                        { id: 1608290515008 },
+                        { id: currAltId },
+                        { id: pilotAirspeedId },
                         // { id: pilotTrueAirspeedId },
-                        { id: 1608290518368 },
-                        { id: 1608290514504 }
+                        { id: compassHeadingDegId },
+                        { id: compassMagneticHeadingId }
                     ]
                 }
             }
             console.log(`Final WS Payload: ${JSON.stringify(payload)}`)
             ws.send(JSON.stringify(payload));
-        });
-        ws.addEventListener("message", (event) => {
-            // console.log("Message from server ", JSON.parse(event.data));
-            const json = JSON.parse(event.data);
-            if ("result" === json.type) {
-                console.log(`Subscribed values ${json.success}!`);
-            }
-            if ("dataref_update_values" === json.type) {
-                setCurrAltFt(getUpdatedDataRefWs(json.data, 1608290516016));
-                // setCurrAltFt(getUpdatedDataRefWs(json.data, currAltId));
-                setCurrSpd(getUpdatedDataRefWs(json.data, 1608290515008));
-                // setCurrSpd(getUpdatedDataRefWs(json.data, pilotAirspeedId));
-                setCurrHdg(getUpdatedDataRefWs(json.data, 1608290518368));
-                // setCurrHdg(getUpdatedDataRefWs(json.data, compassHeadingDegId));
-                setCurrMagHdg(getUpdatedDataRefWs(json.data, 1608290514504));
-                // setCurrMagHdg(getUpdatedDataRefWs(json.data, compassMagneticHeadingId));
-            }
-        });
-    }, [getUpdatedDataRefWs])
-
-    const getDataRefVal = async (id: number | undefined) => {
-        console.log(`DataRefId (${id})`);
-        if (id !== undefined && id > 0) {
-            return await fetch(`${rootUrl}/${id}/value`)
-                .then(res => res.json());
-        } else {
-            return await Promise.resolve({ data: 0 });
+            ws.addEventListener("message", (event) => {
+                console.log("Server Message: ", JSON.parse(event.data));
+                const json = JSON.parse(event.data);
+                if ("result" === json.type) {
+                    console.log(`Response status ${json.success}!`);
+                }
+                if ("dataref_update_values" === json.type) {
+                    setCurrAltFt(getUpdatedDataRefWs(json.data, currAltId, currAltFt));
+                    setCurrSpd(getUpdatedDataRefWs(json.data, pilotAirspeedId, currSpd));
+                    setCurrHdg(getUpdatedDataRefWs(json.data, compassHeadingDegId, currHdg));
+                    setCurrMagHdg(getUpdatedDataRefWs(json.data, compassMagneticHeadingId, currMagHdg));
+                }
+            });
         }
     }
 
-    // TODO: Remove all these after fixing websocket. These will be populated by then.
-    const getAltitude = async () => {
-        console.log('getAltitude()');
-        return getDataRefVal(currAltId);
-    }
-
-    const getAirspeed = async () => {
-        console.log('getAirSpeed');
-        return getDataRefVal(pilotAirspeedId);
-    }
-
-    const getHeading = async () => {
-        console.log('getHeading()');
-        return getDataRefVal(compassHeadingDegId);
-    }
-
-    const getMagHeading = async () => {
-        console.log('getMagHeading()');
-        return getDataRefVal(compassMagneticHeadingId);
-    }
-
-    const connectWs = () => {
-        console.log('connectWs()');
-        ws.addEventListener('open', () => {
-            console.log('Connected to server');
-            const payload: DataRefWsRequest = {
-                req_id: 4,
-                type: 'dataref_subscribe_values',
-                params: {
-                    datarefs: [
-                        { id: 2004938583872 },
-                        { id: 2004938582864 },
-                        // { id: pilotTrueAirspeedId },
-                        { id: 2004938586224 },
-                        { id: 2004938582360 }
-                    ]
-                }
+    const stopWs = () => {
+        const payload: DataRefWsRequest = {
+            req_id: 2,
+            type: 'dataref_unsubscribe_values',
+            params: {
+                datarefs: 'all'
             }
-            console.log(`Final WS Payload: ${JSON.stringify(payload)}`)
+        }
+
+        if (ws.readyState === ws.OPEN) {
+            console.log(JSON.stringify(payload));
             ws.send(JSON.stringify(payload));
-        });
-        ws.addEventListener("message", (event) => {
-            console.log("Message from server ", event.data);
-        });
+        }
+    }
+
+    /**
+     * Call REST API for value of DataRef
+     * @deprecate We use WebSockets for this now.
+     */
+    const getDataRefVal = async (id: number | undefined) => {
+        console.log(`DataRefId (${id})`);
+
+        return await (id !== undefined && id > 0)
+            ? fetch(`${rootUrl}/${id}/value`).then(res => res.json())
+            : Promise.resolve({ data: 0 });
+    }
+
+    const areDataRefIdsReady = (): boolean => {
+        return ws.readyState != ws.OPEN
+            && currAltId === 0
+            && pilotAirspeedId === 0
+            && compassHeadingDegId === 0
+            && compassMagneticHeadingId === 0
     }
 
     useEffect(() => {
@@ -172,50 +186,58 @@ export default function Page() {
         setCompassHeadingDegId(getDataRefSessionId('sim/cockpit2/gauges/indicators/heading_AHARS_deg_mag_pilot'));
         setCompassMagneticHeadingId(getDataRefSessionId('sim/cockpit2/gauges/indicators/compass_heading_deg_mag'));
 
-        getAltitude().then(res => {
-            const ft: number = res.data;
-            const mt = ft * 0.3048;
-            setCurrAltFt(ft);
-            setCurrAltMt(mt);
-        });
-        getAirspeed().then(res => setCurrSpd(res.data.toLocaleString('en-us', { minimumFractionDigits: 2 })))
-        getHeading().then(res => setCurrHdg(res.data));
-        getMagHeading().then(res => setCurrMagHdg(res.data));
+        // getAltitude().then(res => {
+        //     const ft: number = res.data;
+        //     const mt = ft * 0.3048;
+        //     setCurrAltFt(ft);
+        //     setCurrAltMt(mt);
+        // });
+
+        // getAirspeed().then(res => setCurrSpd(res.data.toLocaleString('en-us', { minimumFractionDigits: 2 })))
+        // getHeading().then(res => setCurrHdg(res.data));
+        // getMagHeading().then(res => setCurrMagHdg(res.data));
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [getDataRefSessionId])
 
     useEffect(() => {
         subscribeToDataRefs();
-    }, [subscribeToDataRefs]);
+    }, [subscribeToDataRefs, compassHeadingDegId, compassMagneticHeadingId, currAltId, pilotAirspeedId]);
 
     return (
         <div>
             <h1>Frame</h1>
             <Typography variant="body1">
-                Altitude: {currAltFt}
-                {/* Altitude {Math.round(currAltFt).toLocaleString('en-us', { minimumFractionDigits: 0 })}ft / {Math.round(currAltMt).toLocaleString('en-us', { minimumFractionDigits: 0 })}m */}
+                Test Altitude: {currAltFt}<br />
+                Altitude {Math.round(currAltFt).toLocaleString('en-us', { minimumFractionDigits: 0 })}ft /
+                {Math.round(currAltFt * 0.3048).toLocaleString('en-us', { minimumFractionDigits: 0 })}m
             </Typography>
             <Typography variant="body1">
-                Airspeed {currSpd}kts
-                {/* Airspeed {Math.round(currSpd).toLocaleString('en-us', { minimumFractionDigits: 0 })}deg */}
+                Test Airspeed {currSpd}kts<br />
+                Airspeed {Math.round(currSpd).toLocaleString('en-us', { minimumFractionDigits: 0 })}deg
             </Typography>
             <Typography variant="body1">
+                Test Heading {currHdg}kts <br />
                 Heading {Math.round(currHdg).toLocaleString('en-us', { minimumFractionDigits: 0 })}deg
             </Typography>
             <Typography variant="body1">
+                Test Magnetic Heading {currMagHdg}kts <br />
                 Magnetic North Heading {Math.round(currMagHdg).toLocaleString('en-us', { minimumFractionDigits: 0 })}deg
             </Typography>
-            {/* <Typography variant="body1">
-                DataRefs {currAltId} {pilotAirspeedId}
-                {JSON.stringify(dataRefs)}
-            </Typography> */}
+            <Typography variant="body1">
+                DataRefs {currAltId},
+                {pilotAirspeedId},
+                {compassHeadingDegId},
+                {compassMagneticHeadingId}
+                {/* {JSON.stringify(dataRefs)} */}
+            </Typography>
             <Button
                 variant="contained"
-                // disabled={currAltId != undefined
-                //     && pilotAirspeedId !== undefined
-                //     && compassHeadingDegId !== undefined
-                //     && compassMagneticHeadingId !== undefined}
+                disabled={areDataRefIdsReady()}
                 onClick={connectWs}>Get DataRefs</Button>
+            <Button
+                variant="contained"
+                disabled={areDataRefIdsReady()}
+                onClick={stopWs}>Stop DataRefs</Button>
         </div>
     );
 }
